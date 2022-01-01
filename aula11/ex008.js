990b50e0f13a6581ee5e9f1c39778c641d4ecd6b
Nala Ayaba <83035328+NalaBlackTrava@users.noreply.github.com>
2022-01-01 15:02:17 -0300

    Add files via upload

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 42
+console.log(`A velo do seu carro é ${vel}km/h`)
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando cinto, seu comédia!')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+console.log(`Vida ganha né? Vivendo no ${país} ¯\_( ͠❛ ͜ʖ ͠❛ )_/¯`)
+if (país == 'Brasil') {
+    console.log('Ih, ce é Brasilere kkkkkkk, vida ganha nada, tá fudide menó.')
+} else {
+    console.log('Gringo é fda')
+}
